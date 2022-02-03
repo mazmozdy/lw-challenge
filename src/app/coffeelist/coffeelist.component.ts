27397f13f2cb7ffcd4b3f6618555315f14807b7f
Emad <Emad.Bamatraf@ibm.com>
2022-02-03 17:26:29 -0400

    [updates]

--- src/app/coffeelist/coffeelist.component.ts
+++ src/app/coffeelist/coffeelist.component.ts
@@ -1,10 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
